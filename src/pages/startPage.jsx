c8cc5751c038bc09f353d640410ea44fa9088d5e
Zasada94 <pawelzasada2@wp.pl>
2023-04-12 14:54:02 +0200

    added startPage styling

--- src/pages/startPage.jsx
+++ src/pages/startPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../css/startPage.css";
 
 function StartPage({ onSubmit }) {
 	const [name, setName] = useState("");
@@ -10,15 +11,15 @@ function StartPage({ onSubmit }) {
 	};
 
 	return (
-		<div>
+		<div className="startPage">
 			<h1>Wordcloud Game</h1>
 			<form onSubmit={handleSubmit}>
-					<input
-						type="text"
-						placeholder="Enter your nickname here:"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-					/>
+				<input
+					type="text"
+					placeholder="Enter your nickname here:"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
 			</form>
 			<button type="submit">
 				<Link to="/gamePage">PLAY</Link>

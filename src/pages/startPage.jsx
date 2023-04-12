503d8655433a1bfbb877bfe72e62ee66c08a2a6a
Zasada94 <pawelzasada2@wp.pl>
2023-04-12 14:02:28 +0200

    added form collecting nickname

--- src/pages/startPage.jsx
+++ src/pages/startPage.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-function StartPage() {
+function StartPage({ onSubmit }) {
+	const [name, setName] = useState("");
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		onSubmit(name);
+	};
+
 	return (
 		<div>
-			{" "}
-			<h1>Welcome to My App!</h1>
-			<Link to="/gamePage">Next Page</Link>
+			<h1>Wordcloud Game</h1>
+			<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						placeholder="Enter your nickname here:"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+			</form>
+			<button type="submit">
+				<Link to="/gamePage">PLAY</Link>
+			</button>
 		</div>
 	);
 }

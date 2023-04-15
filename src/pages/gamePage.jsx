3850ce58dfba124e16581f2225cdd39b5cf3aabf
Zasada94 <pawelzasada2@wp.pl>
2023-04-15 13:25:40 +0200

    first version of game added

--- src/pages/gamePage.jsx
+++ src/pages/gamePage.jsx
@@ -1,14 +1,73 @@
-import React from "react";
 import { useParams } from "react-router-dom";
+import React, { useState } from "react";
 
-function GamePage() {
-	const { name } = useParams();
+const API = {
+	question: "select animals",
+	all_words: [
+		"hole",
+		"sofa",
+		"pear",
+		"tiger",
+		"oatmeal",
+		"square",
+		"nut",
+		"cub",
+		"shirt",
+		"tub",
+		"passenger",
+		"cow",
+	],
+	good_words: ["tiger", "cow"],
+};
+
+const GamePage = () => {
+	const [selectedWords, setSelectedWords] = useState([]);
+	const [showAnswers, setShowAnswers] = useState(false);
+
+	const handleWordClick = (word) => {
+		if (selectedWords.includes(word)) {
+			setSelectedWords(selectedWords.filter((w) => w !== word));
+		} else {
+			setSelectedWords([...selectedWords, word]);
+		}
+	};
+
+	const checkAnswers = () => {
+		setShowAnswers(true);
+	};
+
+	const renderWord = (word) => {
+		const isGood = API.good_words.includes(word);
+		const isSelected = selectedWords.includes(word);
+		const isAnswer = showAnswers;
+
+		return (
+			<span
+				key={word}
+				onClick={() => handleWordClick(word)}
+				style={{
+					display: "inline-block",
+					padding: "5px",
+					margin: "5px",
+					backgroundColor: isSelected ? "lightblue" : "white",
+					borderRadius: "5px",
+					cursor: "pointer",
+					color: isSelected && isAnswer ? (isGood ? "green" : "red") : "black",
+				}}
+			>
+				{word}
+			</span>
+		);
+	};
+
 	return (
 		<div>
-			<h1>Next Page</h1>
-			<p>Congratulations {name}, you made it to the next page!</p>
+			<h2>{API.question}</h2>
+			<div>{API.all_words.map((word) => renderWord(word))}</div>
+			<button onClick={checkAnswers}>Sprawdź odpowiedzi</button>
+			{showAnswers}
 		</div>
 	);
-}
+};
 
 export default GamePage;
